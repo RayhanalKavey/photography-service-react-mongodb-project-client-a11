3331293871cc349fd998501183ec8bc725b370de
RayhanalKavey <rayhanalkavey@gmail.com>
2022-11-08 00:53:38 +0600

    Add flowbite and title of the website.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h2>This is the project -11</h2>
+      <h2 className="text-xl font-bold">This is the project -11</h2>
+      <button
+        type="button"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      >
+        Default
+      </button>
+
       <Toaster />
     </div>
   );
