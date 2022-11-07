f4a497c50b8e1840aa6631ecc8c07f938e465418
RayhanalKavey <rayhanalkavey@gmail.com>
2022-11-08 01:15:12 +0600

    Setup router with header,footer component.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import { Toaster } from "react-hot-toast";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/Routes/Routes";
 import "./App.css";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
-    <div>
-      <h2 className="text-xl font-bold">This is the project -11</h2>
-      <button
-        type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-      >
-        Default
-      </button>
+    <div className="max-w-screen-xl	mx-auto">
+      <RouterProvider router={router}></RouterProvider>
 
       <Toaster />
     </div>
